9f69c50e7b96d9c1be1b52e02fe13f117dcde46a
Mohamed Lafrouh <simoarmymen9@gmail.com>
2023-04-16 15:40:27 +0000

    first commit

---
+++ Main.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="images/Discord.png" type="image/png">
+    <script src="Scripts/JQuery.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>Discord Server Dashboard</title>
+</head>
+<body class="bg-dark">
+    <nav class="navbar navbar-expand-lg 
+        bg-dark border border-top-0 
+        border border-primary 
+        border-start-0 border 
+        border-end-0 sticky-top">
+        <div class="container-fluid d-flex flex-row">
+            <h3 class="navbar-brand text-primary">Discord Dashboard Server</h3>
+            <div class="justify-content-end">
+                <button class="btn btn-primary" role="button" onclick="window.location.reload();">Refresh</button>
+            </div>
+        </div>
+    </nav>
+    <main>
+        <?php
+        $widget_url = "https://discord.com/api/guilds/1031300982488977428/widget.json";
+        $widget_json = file_get_contents($widget_url);
+        $widget_data = json_decode($widget_json, true);
+        $member_count = $widget_data["presence_count"];
+        $members = $widget_data["members"]
+        ?>
+        <div class="text-light">
+            <div class="containter-fluid p-5 d-flex flex-column justify-content-between">
+                <h1 class="text-primary">
+                    Discord Members Online :
+                    <?php
+                    echo '<h3>' . $member_count . '</h3>';
+                    echo '<h3 class="text-primary"> Current Online Members : </h3>';
+                    foreach ($members as $member) {
+                        switch ($member['status']) {
+                            case 'dnd':
+                                $colored_status = "&nbsp<span class='text-danger h4'>".$member['status']."</span>";
+                                break;
+                            case 'online':
+                                $colored_status = "&nbsp<span class='text-success h4'>".$member['status']."</span>";
+                                break;
+                            case 'idle':
+                                $colored_status = "&nbsp<span class='text-warning h4'>".$member['status']."</span>";
+                                break;
+                            default:
+                                $colored_status = "&nbsp<span class='text-light h4'>".$member['status']."</span>";
+                                break;
+                            }
+                        $avatar_url = $member['avatar_url'];
+                        
+                        echo "<img src='$avatar_url' class='rounded' style='width:60px;magrin:5px;'/>&nbsp";
+                        echo '<span class="h4 text-primary">Name: </span>'.
+                        '<span class="h4 text-light">'.$member['username'].'</span>&nbsp'.
+                        '<span class="text-primary h4">status:'.'</span>'.$colored_status.'</br><hr class="bg-primary">';
+                    }
+                    ?>
+                </h1>
+            </div>
+        </div>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ README.md
@@ -0,0 +1,39 @@
+# Discord API Information Dashboard
+This application is designed to provide an easy-to-use information dashboard that utilizes the Discord API. With this dashboard, users can quickly access and view a variety of information from their Discord server, including server statistics, user activity, and more.
+
+## Features
+- Server statistics, including member count, activity levels, and more.
+- User activity tracking, including messages sent, voice channel activity, and more.
+Real-time updates and notifications for new messages, voice activity, and other events.
+Customizable dashboard layout and design.
+
+## Requirements
+- A Discord account and server with API access.
+- A web server with PHP and MySQL support.
+- Check IF A Discord server supports Server Widgets.
+- You Must have Php installed in your pc , because the page only work in
+a localhost server.
+
+## Installation
+- Clone or download the application from the GitHub repository.
+- Set up a MySQL database to store user and server information.
+- Configure the application settings in the config.php file.
+- Upload the application files to your web server.
+- Navigate to the application URL to access the dashboard.
+
+
+### Usage
+- Once installed, the application can be used to view a variety of information from your Discord server. 
+- The dashboard includes several panels that provide different types of information:
+
+- Server Statistics: This panel displays the current member count, activity levels, and other server statistics.
+User Activity: This panel displays the activity levels of individual users, including messages sent, voice channel activity, and more.
+- Notifications: This panel displays real-time updates and notifications for new messages, voice activity, and other events.
+You can customize the layout and design of the dashboard by modifying the dashboard.php file.
+- You can also add additional functionality by modifying the application code and integrating with other APIs.
+
+## Support
+If you have any questions or issues with the application, please contact the developer at developer@example.com.
+
+### License
+This application is licensed under the MIT License. See the LICENSE file for more information.
